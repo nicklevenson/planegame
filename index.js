69aa292aa6dc5e33084857a4db563ec7b0cfba84
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-06 14:24:35 -0800

    sequence of angle

--- index.js
+++ index.js
@@ -1,44 +1,61 @@
 const plane = document.getElementById("plane")
 const slide = document.getElementById("slide")
+let doSlide
+let doAngle
+startAngle()
 
-document.addEventListener('keyup', function(e){
-  if (e.code === "Space") {movePlane(); clearTimeout(doSlide)}
-})
+function startAngle() {
+  angleLoop()
+  document.addEventListener('keyup', function space(e){
+    if (e.code === "Space") {
+      clearTimeout(doAngle)
+      sliderLoop()
+      startSlide()
+      document.removeEventListener("keyup", space);
+    }
+  })
+}
+function startSlide() {
+  document.addEventListener('keyup', function(e){
+    if (e.code === "Space") {
+      movePlane() 
+      
+    }
+  })
+}
 
 function movePlane() {
+  clearTimeout(doSlide)
   let distance = slide.clientWidth * 1.5;
-  // plane.style.bottom = currentBottom + distance + "px"
   console.log( 1 / (distance / 1000))
   move = setInterval(() => inchPlane(distance), 1 / (distance / 1000));
 }
 
 function anglePlane(angle = -45, direction = "right") {
-  let tr = window.getComputedStyle(plane).transform
-  var values = tr.split('(')[1],
-  values = values.split(')')[0],
-  values = values.split(',');
-  let b = values[1];
-  angle = Math.round(Math.asin(b) * (180/Math.PI));
-  
+  angle = getAngle(); 
   if (direction === "right") {
     plane.style.transform = `rotate(${angle + 1}deg)`
   }
   if (direction === "left") {
     plane.style.transform = `rotate(${angle - 1}deg)`
   }
-  
   if (angle === -45) {
     direction = "right"
-  }
-    
+  }   
   if (angle === 45) {
     direction = "left"
   }
-
   angleLoop(angle, direction)
-
 }
 
+function getAngle() {
+  let tr = window.getComputedStyle(plane).transform
+  var values = tr.split('(')[1],
+  values = values.split(')')[0],
+  values = values.split(',');
+  let b = values[1];
+  return Math.round(Math.asin(b) * (180/Math.PI)); 
+}
 
 function angleLoop(angle, direction) {
   doAngle = setTimeout(function() {anglePlane(angle, direction)}, 1)
@@ -75,7 +92,5 @@ function slider(direction = "up") {
 function sliderLoop(direction) {
   doSlide = setTimeout(function() {slider(direction)}, 1)
 }
-
-sliderLoop()
 
 
