1e86353dea1140f25df21a1c250b58f831a2819f
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-09 13:40:56 -0800

    game logic

--- game.js
+++ game.js
@@ -1,28 +1,40 @@
-window.addEventListener('load', e=>{newGame()})
-const score = document.getElementById("score")
-const roundTag = document.getElementById("round")
-const control = document.getElementById("control")
-let round = 1;
-let tally = 0;
-function newGame() {
-  if (round < 11) {
-    console.log(round)
-    newRound()
-    roundTag.innerText = `Round: ${round}/10`
-    round ++
-  }else {
-    console.log("done")
-    roundTag.innerText = `Game Over.`
-  }
-}
+window.addEventListener('load', e=>{playGame()})
+  let score = document.getElementById("score")
+  let roundTag = document.getElementById("round")
+  let control = document.getElementById("control")
+  let round = 1;
+  let tally = 0;
 
-function addScore(s) {
-  control.innerText = "Next Round"
-  tally += s
-  score.innerText = `Score: ${tally}`
-  control.addEventListener('click', function restart(){
-    
+  function playGame() {
+    round = 1;
+    tally = 0;
     newGame()
-    control.removeEventListener('click',restart)
-  })
-}
+  }
+  function newGame() {
+    if (round < 11) {
+      console.log(round)
+      newRound()
+      roundTag.innerText = `Round: ${round}/10`
+      round ++
+    }else {
+      console.log("done")
+      roundTag.innerText = `Game Over.`
+      control.innerText = "Play Again?"
+      control.addEventListener('click', function restart(){
+        playGame()
+        control.removeEventListener('click',restart)
+      })
+    }
+  }
+
+  function addScore(s) {
+    control.innerText = "Next Round"
+    tally += s
+    score.innerText = `Score: ${tally}`
+    control.addEventListener('click', function restart(){
+      
+      newGame()
+      control.removeEventListener('click',restart)
+    })
+  }
+
