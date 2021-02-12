390a19c7a9cb076a2ba0b1d2a6ea7a3ff588421a
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-12 10:55:48 -0800

    intro card

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ class introCard{
     let card = document.createElement('div')
     card.className = "introCard"
     newUserContainer.append(card)
-    card.innerHTML = `This is the story of ${this.username}.`
-    card.innerHTML +=  `<button id="ready-to-fly">Ready to fly.</button>`
+    card.innerHTML = `This is the story of ${this.username}. One day ${this.username} was bored at the park. ${this.username} had a notebook of paper and a quilt in their bag.  ${this.username} laid the quilt on the ground and noticed it looked an awful lot like a target. It dawned on ${this.username} that the perfect way to spend this lovely afternoon was to throw a paper airplane at this target...`
+    card.innerHTML +=  `<br><button id="ready-to-fly">Ready to fly.</button>`
     let button = document.getElementById('ready-to-fly')
     button.addEventListener("click", (e) => {
       usernameContainer.remove()
